b2a2d7507f0c15638bfb8b0917824c2cc340c8d1
jeffsyn <jeff.phoenix.syn@gmail.com>
2024-12-04 22:35:15 +0530

    Amending cartTotal and result in index.js

--- index.js
+++ index.js
@@ -14,8 +14,7 @@ let loyaltyRate = 2; // 2 points per $1
 app.get("/cart-total", (req, res) => {
   let newItemPrice = parseFloat(req.query.newItemPrice);
   let cartTotal = parseFloat(req.query.cartTotal);
-  let totalPrice = newItemPrice + cartTotal;
-  let result = "Total price of items in your cart is Rs. " + totalPrice + "/-";
+  let result = "Total price of items in your cart is Rs. " + newItemPrice + cartTotal + "/-";
   res.send(result.toString());
 });
 /// API Call - https://stackblitzstartersa9gnkk-34f5--3000--fc837ba8.local-credentialless.webcontainer.io/cart-total?newItemPrice=1200&cartTotal=0
